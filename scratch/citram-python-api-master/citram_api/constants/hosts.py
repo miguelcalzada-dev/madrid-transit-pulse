35cd65b4767be11c3ebcd180e82aba9c440eb49d
miguelcalzada-dev <99368388+miguelcalzada-dev@users.noreply.github.com>
2026-09-16 09:42:47 +0200

    chore: remove scratch/vendored code and build artifacts

--- scratch/citram-python-api-master/citram_api/constants/hosts.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class Urls(Enum):
-    CITRAM_WIDGET_SERVICE = 'https://www.crtm.es/widgets/api'
-    CITRAM_CARD_SERVICE = 'http://www.citram.es:50081/VENTAPREPAGOTITULO/VentaPrepagoTitulo.svc?wsdl'
